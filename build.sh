4631028d0196139298c52c7f961eb8fdf4746292
Jason R. McNeil <jason@jasonrm.net>
2016-04-14 08:13:34 -0700

    pull latest image

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o pipefail
 IMAGE_NAME="atomica/arch-base"
 
 # Do the things that we can only do in docker build
-docker build --force-rm --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" --tag="${IMAGE_NAME}:latest" .
+docker build --pull --force-rm --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" --tag="${IMAGE_NAME}:latest" .
 
 # Test that it can be ran
 docker run --rm "${IMAGE_NAME}:latest" /bin/env
